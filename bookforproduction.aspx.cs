d03def20a6fba8a94603e03d5131b67dc0d08cd4
VishalKoshti <VishalKoshti@VISHAL>
2022-07-11 16:47:06 +0530

    Qty Available Working in Book Order

--- bookforproduction.aspx.cs
+++ bookforproduction.aspx.cs
@@ -30,7 +30,7 @@ namespace MorphleInventorymanagementSystem
 
         protected void submit_Click(object sender, EventArgs e)
         {
-            SqlCommand command = new SqlCommand("select Part_Number,Part_Discription,Category,(C240*"+c240.Text+") as [C240 Batch],(Optimus*"+optimus.Text+ ") as [Optimus Batch],(H1*" + h1.Text + ") as [H1 Batch],(H6*" + h6.Text + ") as [H6 Batch],(indexscanner*" + index.Text + ") as [Index Batch],(Brain*" + brain.Text + ") as [Brain Batch], ((C240*" + c240.Text + ")+(Optimus*" + optimus.Text + ")+(H1*" + h1.Text + ")+(H6*" + h6.Text + ")+(indexscanner*" + index.Text + ")+(Brain*" + brain.Text + "))as [Total Qty Required]from BOM", con);
+            SqlCommand command = new SqlCommand("select BOM.Part_Number,BOM.Part_Discription,BOM.Category,(BOM.C240*" + c240.Text+ ") as [C240 Batch],(BOM.Optimus*" + optimus.Text+ ") as [Optimus Batch],(BOM.H1*" + h1.Text + ") as [H1 Batch],(BOM.H6*" + h6.Text + ") as [H6 Batch],(BOM.indexscanner*" + index.Text + ") as [Index Batch],(BOM.Brain*" + brain.Text + ") as [Brain Batch], ((BOM.C240*" + c240.Text + ")+(BOM.Optimus*" + optimus.Text + ")+(BOM.H1*" + h1.Text + ")+(BOM.H6*" + h6.Text + ")+(BOM.indexscanner*" + index.Text + ")+(BOM.Brain*" + brain.Text + ")) as [Total Qty Required], StockAvailability.[Qty Available] from BOM inner join StockAvailability on BOM.Part_Number=StockAvailability.Part_Number", con);
             SqlDataAdapter sd = new SqlDataAdapter(command);
             DataTable dt = new DataTable();
             sd.Fill(dt);
